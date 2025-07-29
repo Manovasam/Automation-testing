02d11ad757db7ccb7756217b4e90d4c88fd50a3d
Manova sam <155352692+Manovasam@users.noreply.github.com>
2025-07-29 22:49:24 +0530

    Update Task - 1.py

--- Task - 1.py
+++ Task - 1.py
@@ -3,33 +3,27 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 import time
 
-#Setup browser
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get("https://www.saucedemo.com/")
 
-#Login
 driver.find_element(By.ID, "user-name").send_keys("standard_user")
 driver.find_element(By.ID, "password").send_keys("secret_sauce")
 driver.find_element(By.ID, "login-button").click()
 time.sleep(2)
 
-#Sort products by Price (low to high)
 select = Select(driver.find_element(By.CLASS_NAME, "product_sort_container"))
 select.select_by_visible_text("Price (low to high)")
 time.sleep(2)
 
-#Get product details
 first_product = driver.find_elements(By.CLASS_NAME, "inventory_item")[0]
 product_name = first_product.find_element(By.CLASS_NAME, "inventory_item_name").text
 product_price = first_product.find_element(By.CLASS_NAME, "inventory_item_price").text
-first_product.find_element(By.TAG_NAME, "button").click()
+first_product.find_element(By.TAG_NAME, "button").click() 
 
-#Go to Cart
 driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
 time.sleep(2)
 
-#Verify name, price, quantity
 cart_item = driver.find_element(By.CLASS_NAME, "cart_item")
 cart_name = cart_item.find_element(By.CLASS_NAME, "inventory_item_name").text
 cart_price = cart_item.find_element(By.CLASS_NAME, "inventory_item_price").text
