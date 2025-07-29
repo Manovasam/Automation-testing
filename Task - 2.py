adb63444b5d2acd4df8b7b1024bd2778ff8f435d
Manova sam <155352692+Manovasam@users.noreply.github.com>
2025-07-29 22:49:53 +0530

    Update Task - 2.py

--- Task - 2.py
+++ Task - 2.py
@@ -3,12 +3,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 import time
 
-# Setup
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get("https://www.saucedemo.com/")
 
-# Login
 driver.find_element(By.ID, "user-name").send_keys("error_user")
 driver.find_element(By.ID, "password").send_keys("secret_sauce")
 driver.find_element(By.ID, "login-button").click()
